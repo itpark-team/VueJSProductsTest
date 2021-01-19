fb3a87e1069e08105c13b0bd7aced5f0a263a68a
itpark-team <46758840+itpark-team@users.noreply.github.com>
2021-01-19 23:03:12 +0300

    done insert

--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -7,8 +7,17 @@
             </li> 
         </ul>
         <div>
-            <button v-on:click="loadProducts()">Load products</button>
+            <button v-on:click="getProducts()">Load products</button>
         </div>
+
+        <div>
+            <label><input type="text" v-model="product.name"></label>
+        </div>
+
+        <div>
+            <button v-on:click="postProduct()">Add new product</button>
+        </div>
+
     </div>
 </template>
 
@@ -16,29 +25,94 @@
 export default {
     data(){
         return{
-            products:[]
+            products:[],
+
+            product:{
+                name:"",
+                IdCategory:1
+            }
         }
     },
     methods:{
-        deleteProduct(id){
-
-            let index = this.products.findIndex(item => item.id==id);
-
-            if(index!=-1)
+        async getProducts()
+        {
+            try
             {
-                this.products.splice(index, 1);
+                let response = await fetch("https://localhost:44368/api/Products",{
+                    method:"GET"
+                });
+
+                if(response.ok==true)
+                {
+                    this.products = await response.json();
+                }
+                else
+                {
+                    alert(response.status+": "+response.statusText)
+                }
+            }
+            catch(error)
+            {
+                console.log("Error: "+error);
             }
         },
-        async loadProducts(){
-            let response = await fetch("https://localhost:44368/api/Products");
 
-            if(response.ok==true)
+        async deleteProduct(id)
+        {
+            try
             {
-                this.products = await response.json();
+                let response = await fetch("https://localhost:44368/api/Products/"+id,{
+                    method:"DELETE"
+                });
+
+                if(response.ok==true)
+                {
+                    let recievedId = await response.json();
+
+                    //this.products.filter(item=>item.id != recievedId);
+
+                    let index = this.products.findIndex(item => item.id==recievedId);
+                    this.products.splice(index, 1);                   
+                }
+                else
+                {
+                    alert(response.status+": "+response.statusText)
+                }
             }
-            else
+            catch (error)
             {
-                console.log("errrror");
+                console.log("Error: "+error);
+            }            
+        },
+
+        async postProduct()
+        {
+            try
+            {
+                let response = await fetch("https://localhost:44368/api/Products/",{
+                    method:"POST",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.product)
+                });
+
+                if(response.ok==true)
+                {
+                    let recievedProduct = await response.json();
+
+                    this.products.push(recievedProduct);
+
+                    this.product.name = "";
+                }
+                else
+                {
+                    alert(response.status+": "+response.statusText)
+                }
+            }
+            catch (error) 
+            {
+                console.log("Error: "+error);   
             }
         }
     }
